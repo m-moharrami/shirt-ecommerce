b715d7c23b2a7f23e873b31bd681154cd655db44
m-moharrami <moharram.m98@gmail.com>
2023-01-12 00:16:07 +0330

    connect and disconnect to the mongodb database

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <h1>hi there!</h1>
         </>
     );
 }
