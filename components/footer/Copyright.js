2e3d38a7a7f9138a45f00995c79fd353ac246b34
m-moharrami <moharram.m98@gmail.com>
2023-01-23 17:11:59 +0330

    made layout and style payment methods and copyright sections in footer

---
+++ components/footer/Copyright.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+import { IoLocationSharp } from "react-icons/io5";
+
+export default function Copyright() {
+    return (
+        <div className={styles.footer__copyright}>
+            <section>© 2023 My T-shirt Land All Rights Reserved</section>
+            <section>
+                <ul>
+                    {data.map((link) => (
+                        <li>
+                            <Link href={link.link}>{link.name}</Link>
+                        </li>
+                    ))}
+                    {/* This part below is for showing location, which we won't use in over final project */}
+                    <li>
+                        <a>
+                            <IoLocationSharp /> Iran/Tabriz
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    );
+}
+
+const data = [
+    {
+        name: "Privacy Center",
+        link: "",
+    },
+    {
+        name: "Privacy & Cookie Policy",
+        link: "",
+    },
+    {
+        name: "Manage Cookies",
+        link: "",
+    },
+    {
+        name: "Terms & Conditions",
+        link: "",
+    },
+    {
+        name: "Copyright Notice",
+        link: "",
+    },
+];
