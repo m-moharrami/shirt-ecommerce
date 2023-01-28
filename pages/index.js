f7316095d64fe4c54a903ec2f5186f2fe8d21b08
m-moharrami <moharram.m98@gmail.com>
2023-01-28 16:57:15 +0330

    setup next auth

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,15 @@ import styles from "../styles/Home.module.scss";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import axios from "axios";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home({ country }) {
+    const { data: session } = useSession();
     return (
         <div>
             <Header country={country} />
+            {/* line below is for test purpose */}
+            {/* {session ? "you are logged in" : "you are not logged in"} */}
             <Footer country={country} />
         </div>
     );
