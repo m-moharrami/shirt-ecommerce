7a096c5bc6e757cd452e91659cb0cf9f1896f782
m-moharrami <moharram.m98@gmail.com>
2023-01-29 12:36:31 +0330

    main structure for homepage

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,20 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import axios from "axios";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Main from "../components/home/main";
 
 export default function Home({ country }) {
     const { data: session } = useSession();
     return (
-        <div>
+        <>
             <Header country={country} />
-            {/* line below is for test purpose */}
-            {/* {session ? "you are logged in" : "you are not logged in"} */}
+            <div className={styles.home}>
+                <div className={styles.container}>
+                    <Main />
+                </div>
+            </div>
             <Footer country={country} />
-        </div>
+        </>
     );
 }
 
